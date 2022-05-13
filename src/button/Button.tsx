5f8baf6ee7e8a1731cbc91c908adcd045e99e12f
Miguel Julio <fitfab@yahoo.com>
2022-05-13 17:28:42 -0400

    tailwind setup

--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -1,5 +1,13 @@
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 export const Button = ({ children, ...props }: ButtonProps) => {
-  return <button {...props}>{children}</button>;
+  //
+  return (
+    <button
+      className=" bg-slate-400 hover:bg-slate-600 text-clip text-white uppercase px-4 flex justify-center items-center min-w-[5rem] max-w-xs h-10 rounded-full"
+      {...props}
+    >
+      {children}
+    </button>
+  );
 };
