e46e2482b776cbf3ea104b158f7535a7acda457a
Miguel Julio <fitfab@yahoo.com>
2022-05-14 14:01:24 -0400

    refactor: update stories to MDX and tweak button styles

--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -1,15 +1,20 @@
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   /**
-   * It indicates if the button is primary or not.
+   * It indicates if the button is primary or not and
+   * it defaults to true.
    */
   primary?: boolean;
+  /**
+   * The content of the button.
+   */
+  children?: React.ReactNode;
 }
 export const Button = ({ children, primary = true, ...props }: ButtonProps) => {
   const variant = primary
     ? "bg-black hover:bg-neutral-800"
-    : "bg-slate-400 hover:bg-slate-600";
-  const btnClasses = `text-clip text-white uppercase px-4 flex justify-center items-center min-w-[5rem] max-w-xs h-10 rounded-full ${variant}`;
+    : "bg-slate-500 hover:bg-slate-600";
+  const btnClasses = `text-white tracking-wide uppercase px-4 truncate min-w-[5rem] max-w-xs h-10 rounded-full ${variant}`;
   return (
     <button className={btnClasses} {...props}>
       {children}
